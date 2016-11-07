dcb56efdda51fb07cbaa7aee54202921e5d530fd
konradko <konrad.korzel@gmail.com>
2016-11-07 14:53:45 +0000

    Remove non-api tests

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,4 @@
 # flake8: noqa
-from tests.locust.test_ui import (
-    RegularUserUI
-)
-from tests.locust.test_sso import (
-    RegularUserSSO,
-    AuthenticatedUserSSO
-)
 from tests.locust.test_api import (
     RegularUserAPI,
     AuthenticatedUserAPI
